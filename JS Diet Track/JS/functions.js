93ed8731d6405bc02e0e412df77f78a9c269b492
Billy <hchoi92@hotmail.com>
2018-06-01 20:26:56 -0700

    Major overhaul of localStorage JSON structure to only create one storage object

--- JS Diet Track/JS/functions.js
+++ JS Diet Track/JS/functions.js
@@ -1,29 +1,42 @@
+var TRACKER_USERS = "trackerUsers";
+
 function loadUsers(){
 var dropDownList = document.getElementById('userDropDown');
 while (dropDownList.hasChildNodes()) {
   dropDownList.removeChild(dropDownList.firstChild);
 }
-  for (var i = 0; i < localStorage.length; i++){
+var usersList = JSON.parse(localStorage.getItem(TRACKER_USERS));
+  for (var i = 0; i < usersList.length; i++){
     var newOption = document.createElement("option");
-        newOption.text = localStorage.key(i);
-        newOption.value = localStorage.key(i);
+        newOption.text = usersList[i].name;
+        newOption.value = usersList[i].name;
         dropDownList.appendChild(newOption)
   }
-
-
 }
+
 
 function drawChart(){
 
+  var chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(231,233,237)'
+  };
+
 var name = document.getElementById('userDropDown').value;
 var chart = document.getElementById('chart');
-var dataEntries = JSON.parse(localStorage.getItem(name));
+var dataEntries = JSON.parse(localStorage.getItem(TRACKER_USERS));
+var index= dataEntries.findIndex(obj => obj.name == name);
 var dateEntries = [];
 var weightEntries = [];
 
-for (var i = 0; i < dataEntries.length; i++){
-  dateEntries.push(dataEntries[i].date);
-  weightEntries.push(dataEntries[i].weight);
+for (var i = 0; i < dataEntries[index]['data'].length; i++){
+  dateEntries.push(dataEntries[index]['data'][i].date);
+  weightEntries.push(dataEntries[index]['data'][i].weight);
 }
 
 let lineChart = new Chart(chart, {
@@ -32,7 +45,8 @@ let lineChart = new Chart(chart, {
     labels: dateEntries,
     datasets:[
       {
-
+        backgroundColor: chartColors.red,
+        borderColor: chartColors.red,
         label: name,
         fill: false,
         data: weightEntries
@@ -57,25 +71,45 @@ function saveEntry(){
   var weight = document.getElementById('weight').value;
   var date = document.getElementById('date').value;
   var issueId = chance.guid();
+  var existsFlag = false;
 
   var newEntry = {
-    name: name,
-    weight: weight,
-    date: date,
-    id: issueId
+    name:name,
+    "data":[{
+      weight: weight,
+      date: date,
+      id: issueId
+    }]
   };
 
-  if (localStorage.getItem(name) == null){
+ var rawData = {
+   weight: weight,
+   date: date,
+   id: issueId
+ }
+
+
+  if (localStorage.getItem(TRACKER_USERS) == null){
     var entries = [];
-    entries.push (newEntry);
-    localStorage.setItem(name, JSON.stringify(entries));
+    entries.push(newEntry);
+    var existsFlag = false;
+    localStorage.setItem(TRACKER_USERS, JSON.stringify(entries));
   } else {
-    var existingEntries = JSON.parse(localStorage.getItem(name));
+    var existingEntries = JSON.parse(localStorage.getItem(TRACKER_USERS));
+    for (var i = 0;i < existingEntries.length; i++){
+        if (name == existingEntries[i].name){
+          existingEntries[i]["data"].push(rawData);
+          existsFlag = true;
+        }
+    }
+  if (existsFlag == false){
     existingEntries.push(newEntry);
-    localStorage.setItem(name, JSON.stringify(existingEntries));
+  }
+    localStorage.setItem(TRACKER_USERS, JSON.stringify(existingEntries));
   }
 loadUsers();
 }
+
 
 function loadEntries(){
   var table = document.getElementById('entryTable');
@@ -87,17 +121,19 @@ function loadEntries(){
      '</thead>';
   var entries = [];
   var name = document.getElementById('userDropDown').value;
-  entries = JSON.parse(localStorage.getItem(name));
-  for (var i = 0; i < entries.length; i ++){
+  entries = JSON.parse(localStorage.getItem(TRACKER_USERS));
+  var index= entries.findIndex(obj => obj.name == name);
+  for (var i = 0; i < entries[index]["data"].length; i ++){
       var newRow = table.insertRow(table.length),
       cell1 = newRow.insertCell(0),
       cell2 = newRow.insertCell(1),
       cell3 = newRow.insertCell(2);
       cell4 = newRow.insertCell(3);
-      cell1.innerHTML = entries[i].name;
-      cell2.innerHTML = entries[i].weight;
-      cell3.innerHTML = entries[i].date;
-      cell4.innerHTML = '<input type = "button" onclick="deleteEntry(\''+ entries[i].id +'\')" class="btn btn-danger" value = "Delete">';
+      cell1.innerHTML = name;
+      cell2.innerHTML = entries[index]['data'][i].weight;
+      cell3.innerHTML = entries[index]['data'][i].date;
+      cell4.innerHTML = '<input type = "button" onclick="deleteEntry(\''+ entries[index]['data'][i].id +'\')" class="btn btn-danger" value = "Delete">';
+      cell4.innerHTML = '<input type = "button" onclick="deleteEntry(\''+ entries[index]['data'][i].id +'\')" class="btn btn-danger" value = "Delete">';
 
 }
 drawChart();
@@ -106,16 +142,19 @@ drawChart();
 
 
 function deleteEntry(id){
-  var name = document.getElementById('userDropDown').value;
-  var entries = JSON.parse(localStorage.getItem(name));
+
+  var entries = JSON.parse(localStorage.getItem(TRACKER_USERS));
   for (var i = 0; i < entries.length; i++){
-  if (id == entries[i].id)  {
-  entries.splice(i,1);
+    for (var j = 0; j < entries[i]["data"].length; j++){
+  if (id == entries[i]["data"][j].id)  {
+  entries[i]["data"].splice(j,1);
   }
-  localStorage.setItem(name, JSON.stringify(entries));
+}
+}
+  localStorage.setItem(TRACKER_USERS, JSON.stringify(entries));
   loadEntries();
 }
-}
+
 
 var deleteUserButton = document.getElementById('deleteUserButton');
 deleteUserButton.addEventListener("click", deleteUser);
@@ -123,3 +162,5 @@ var addEntriesButton = document.getElementById('addEntryButton');
 addEntriesButton.addEventListener("click", saveEntry);
 var loadEntriesButton = document.getElementById('loadEntriesButton');
 loadEntriesButton.addEventListener("click", loadEntries);
+var selectList = document.getElementById('userDropDown');
+selectList.addEventListener("change", loadEntries);
